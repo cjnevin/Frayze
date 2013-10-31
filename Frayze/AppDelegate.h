2fddda262e725c3d182f60f9f24719332ee008f5
CJNevin <chriso@Chrisos-MacBook-Pro.local>
2013-11-01 06:11:39 +1000

    Initial Commit

---
+++ Frayze/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Frayze
+//
+//  Created by CJNevin on 1/11/2013.
+//  Copyright (c) 2013 CJNevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
